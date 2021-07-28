53d4de11c173e6f6d7741b66c2dee1f1d4f8c046
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-28 09:17:32 +0800

    print_chars

--- print_chars.c
+++ print_chars.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
  * print_c - prints a char
  * @c: char to print
@@ -8,9 +9,10 @@
  */
 int print_c(va_list c)
 {
-char ch = (char)va_arg(c, int);
-_putchar(ch);
-return (1);
+	char ch = (char)va_arg(c, int);
+
+	_putchar(ch);
+	return (1);
 }
 
 /**
@@ -21,17 +23,16 @@ return (1);
  */
 int print_s(va_list s)
 {
-int count;
-char *str = va_arg(s, char *);
-if (str == NULL)
-{
-str = "(null)";
-}
-for (count = 0; str[count]; count++)
-{
-_putchar(str[count]);
-}
-return (count);
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
 }
 
 /**
@@ -42,21 +43,20 @@ return (count);
  */
 static int hex_print(char c)
 {
-int count;
-char diff = 'A' - ':';
-char d[2];
-d[0] = c / 16;
-d[1] = c % 16;
-for (count = 0; count < 2; count++)
-{
-if (d[count] >= 10)
-{
-_putchar('0' + diff + d[count]);
-}
-else
-_putchar('0' + d[count]);
-}
-return (count);
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
 }
 
 /**
@@ -67,29 +67,28 @@ return (count);
  */
 int print_S(va_list S)
 {
-unsigned int i;
-int count = 0;
-char *str = va_arg(S, char *);
-if (str == NULL)
-{
-str = "(null)";
-}
-for (i = 0; str[i]; i++)
-{
-if (str[i] < 32 || str[i] >= 127)
-{
-_putchar('\\');
-_putchar('x');
-count += 2;
-count += hex_print(str[i]);
-}
-else
-{
-_putchar(str[i]);
-count++;
-}
-}
-return (count);
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(S, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**
@@ -100,19 +99,18 @@ return (count);
  */
 int print_r(va_list r)
 {
-char *str;
-int i, count = 0;
-str = va_arg(r, char *);
-if (str == NULL)
-{
-str = ")llun(";
-}
-for (i = 0; str[i]; i++)
-;
-for (i -= 1; i >= 0; i--)
-{
-_putchar(str[i]);
-count++;
-}
-return (count);
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
 }
