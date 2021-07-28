41ed1b5489aa35fbf743e4ea45662fe73d8a6dc5
Oluwatobi Abass <72945520+Tobi-Archademy@users.noreply.github.com>
2021-07-28 03:57:20 +0100

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,130 +1,79 @@
 #include "holberton.h"
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * print_i - prints an integer
+ * @i: integer to print
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: number of chars and digits printed
  */
-int _isdigit(int c)
+int print_i(va_list i)
 {
-	return (c >= '0' && c <= '9');
-}
+	int a[10];
+	int j, m, n, sum, count;
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * print_number - prints a number with options
- * @str: the base number as a string
- * @params: the parameter struct
- *
- * Return: chars printed
- */
-int print_number(char *str, params_t *params)
-{
-	unsigned int i = _strlen(str);
-	int neg = (!params->unsign && *str == '-');
-
-	if (!params->precision && *str == '0' && !str[1])
-		str = "";
-	if (neg)
+	n = va_arg(i, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
 	{
-		str++;
-		i--;
+		m /= 10;
+		a[j] = (n / m) % 10;
 	}
-	if (params->precision != UINT_MAX)
-		while (i++ < params->precision)
-			*--str = '0';
-	if (neg)
-		*--str = '-';
-
-	if (!params->minus_flag)
-		return (print_number_right_shift(str, params));
-	else
-		return (print_number_left_shift(str, params));
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
 
 /**
- * print_number_right_shift - prints a number with options
- * @str: the base number as a string
- * @params: the parameter struct
+ * print_d - prints a decimal
+ * @d: decimal to print
  *
- * Return: chars printed
+ * Return: number of chars and digits printed
  */
-int print_number_right_shift(char *str, params_t *params)
+int print_d(va_list d)
 {
-	unsigned int n = 0, neg, neg2, i = _strlen(str);
-	char pad_char = ' ';
+	int a[10];
+	int j, m, n, sum, count;
 
-	if (params->zero_flag && !params->minus_flag)
-		pad_char = '0';
-	neg = neg2 = (!params->unsign && *str == '-');
-	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
-		str++;
-	else
-		neg = 0;
-	if ((params->plus_flag && !neg2) ||
-		(!params->plus_flag && params->space_flag && !neg2))
-		i++;
-	if (neg && pad_char == '0')
-		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
-		n += _putchar('+');
-	else if (!params->plus_flag && params->space_flag && !neg2 &&
-		!params->unsign && params->zero_flag)
-		n += _putchar(' ');
-	while (i++ < params->width)
-		n += _putchar(pad_char);
-	if (neg && pad_char == ' ')
-		n += _putchar('-');
-	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
-		n += _putchar('+');
-	else if (!params->plus_flag && params->space_flag && !neg2 &&
-		!params->unsign && !params->zero_flag)
-		n += _putchar(' ');
-	n += _puts(str);
-	return (n);
-}
-
-/**
- * print_number_left_shift - prints a number with options
- * @str: the base number as a string
- * @params: the parameter struct
- *
- * Return: chars printed
- */
-int print_number_left_shift(char *str, params_t *params)
-{
-	unsigned int n = 0, neg, neg2, i = _strlen(str);
-	char pad_char = ' ';
-
-	if (params->zero_flag && !params->minus_flag)
-		pad_char = '0';
-	neg = neg2 = (!params->unsign && *str == '-');
-	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
-		str++;
-	else
-		neg = 0;
-
-	if (params->plus_flag && !neg2 && !params->unsign)
-		n += _putchar('+'), i++;
-	else if (params->space_flag && !neg2 && !params->unsign)
-		n += _putchar(' '), i++;
-	n += _puts(str);
-	while (i++ < params->width)
-		n += _putchar(pad_char);
-	return (n);
+	n = va_arg(d, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
